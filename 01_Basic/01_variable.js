af1b4faa4b98a7bde8ccfa1f64775ec2b4e56db2
Dhruvsingh7310 <dhruvrajput716@gmail.com>
2026-06-11 08:06:15 +0000

    datatype

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 14432
+let accountEmail = "dhruvsingh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2
+accountEmail = "fs@gam.com"
+accountPassword = "123211"
+accountCity = "banglluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState]);
+
+
---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //traet all JS as newer version
+
+console.log("Dhruv");
+
+let name = "Dhruv"
+let age = "22"
+let isLoggedIn = false
+let state;
+
+
+/*
+number=>2 to power 53
+bigint
+string =>""
+boolean ==>true/false
+null =>standalonne value\
+undefined=>
+symbol =>unique
+object 
+*/
+
+console.log(typeof null);
+
+
